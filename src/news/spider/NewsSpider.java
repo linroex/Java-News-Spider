43d3a3c6005c1daf05a4b81c1f98cdbdfcd4227d
linroex <linroex@coder.tw>
2015-12-07 14:31:11 +0800

    init

---
+++ src/news/spider/NewsSpider.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package news.spider;
+
+/**
+ *
+ * @author linroex
+ */
+public class NewsSpider {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
