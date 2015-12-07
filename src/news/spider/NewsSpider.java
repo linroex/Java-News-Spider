54c838c102bba2cc5b1cde008715623295efc4f9
linroex <linroex@coder.tw>
2015-12-07 15:01:51 +0800

    setTargetSelector method addEnterance method

--- src/news/spider/NewsSpider.java
+++ src/news/spider/NewsSpider.java
@@ -5,17 +5,41 @@
  */
 package news.spider;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 /**
  *
  * @author linroex
  */
 public class NewsSpider {
+    
+    private ArrayList<String> enterance;
+    private String targetSelector;
+    
+    public NewsSpider() {
+        this.enterance = new ArrayList();
+        this.targetSelector = "";
+    }
+    
+    public void setTargetSelector(String selector) {
+        this.targetSelector = selector;
+    }
+    
+    public int addEnterance(String url) {
+        this.enterance.add(url);
+        return this.enterance.size();
+    }
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+
     }
     
 }
